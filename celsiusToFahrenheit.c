4612be9eab33ea1f60c796fd994c601b24cba856
7ejasVijay <7ejas.vijay@gmail.com>
2024-03-30 18:45:51 +0530

    refactor functions

--- celsiusToFahrenheit.c
+++ celsiusToFahrenheit.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+#define UPPER 300
+#define LOWER 0
+#define STEP 20
+
 int main() {
 
   int celsius;
 
-  for (celsius = 0; celsius <= 300; celsius += 20) {
+  for (celsius = LOWER; celsius <= UPPER; celsius += STEP) {
     printf("%3d %6.1f\n", celsius, (1.8 * celsius) + 32);
   }
 
