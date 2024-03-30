e121749e5bcf608239de3652dd8b04acd886a6d0
7ejasVijay <7ejas.vijay@gmail.com>
2024-03-30 18:29:28 +0530

    refactored functions

--- celsiusToFahrenheit.c
+++ celsiusToFahrenheit.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
 
 int main() {
-  float fahr, celsius;
-  float lower, upper, step;
-  
-  lower = 0;
-  upper = 300;
-  step = 20;
 
-  celsius = lower;
+  int celsius;
 
-  while (celsius <= upper) {
-    fahr = (9.0 / 5.0) * (celsius + 32);
-    printf("%3.0f %6.1f\n", celsius, fahr);
-    celsius += step;
+  for (celsius = 0; celsius <= 300; celsius += 20) {
+    printf("%3d %6.1f\n", celsius, (1.8 * celsius) + 32;
   }
 
   return 0;
