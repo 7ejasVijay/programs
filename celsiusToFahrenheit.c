4c8f66798a96723cf71f53c3f64c985cfa3aec34
7ejasVijay <7ejas.vijay@gmail.com>
2024-03-30 18:33:01 +0530

    fixed parenthesis

--- celsiusToFahrenheit.c
+++ celsiusToFahrenheit.c
@@ -5,7 +5,7 @@ int main() {
   int celsius;
 
   for (celsius = 0; celsius <= 300; celsius += 20) {
-    printf("%3d %6.1f\n", celsius, (1.8 * celsius) + 32;
+    printf("%3d %6.1f\n", celsius, (1.8 * celsius) + 32);
   }
 
   return 0;
